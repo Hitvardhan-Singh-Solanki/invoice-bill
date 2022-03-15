450415047b8cdb4e4c1b6c664be9ff5cd7f0e8a5
Hitvardhan Singh Solanki <15267798+Hitvardhan-Singh-Solanki@users.noreply.github.com>
2022-03-16 01:04:54 +0530

    sending data to generate bill window

--- js/generate-bill.js
+++ js/generate-bill.js
@@ -1,11 +1,22 @@
 const { ipcRenderer } = require("electron");
 window.addEventListener("DOMContentLoaded", () => {
   const printInvoiceBtn = document.getElementById("print-invoice-btn");
+  const invoiceTableBody = document.getElementById("invoice-table-body");
   printInvoiceBtn.addEventListener("click", (e) => {
     e.preventDefault();
     window.print();
   });
   ipcRenderer.on("invoice-data", (event, invoiceData) => {
     console.log(invoiceData);
+    invoiceData.forEach((item) => {
+      const tr = document.createElement("tr");
+      Object.entries(item).forEach(([k, v]) => {
+        const td = document.createElement("td");
+        const text = document.createTextNode(v);
+        td.appendChild(text);
+        tr.appendChild(td);
+      });
+      invoiceTableBody.appendChild(tr);
+    });
   });
 });
--- js/index.js
+++ js/index.js
@@ -1,65 +1,65 @@
-const { ipcRenderer } = require('electron');
+const { ipcRenderer } = require("electron");
 
 const tableValues = {
-  material: '',
-  hsn: '',
+  material: "",
+  hsn: "",
   qty: 0,
   price: 0,
-  total: 0
+  total: 0,
 };
 
 const tableData = [];
 
-window.addEventListener('DOMContentLoaded', () => {
-  const searchPartWin = document.querySelector('#search-part-main-btn');
-  const addFileWin = document.querySelector('#add-file-main-btn');
-  const formDOM = document.querySelector('form');
-  const generateBillBtn = document.querySelector('#generate-bill-btn');
+window.addEventListener("DOMContentLoaded", () => {
+  const searchPartWin = document.querySelector("#search-part-main-btn");
+  const addFileWin = document.querySelector("#add-file-main-btn");
+  const formDOM = document.querySelector("form");
+  const generateBillBtn = document.querySelector("#generate-bill-btn");
 
-  searchPartWin.addEventListener('click', searchPartWinHandler);
-  addFileWin.addEventListener('click', addFileWinHandler);
-  generateBillBtn.addEventListener('click', generateBillWinHandler);
-  formDOM.addEventListener('submit', (e) => submitEventHandler(e, formDOM));
+  searchPartWin.addEventListener("click", searchPartWinHandler);
+  addFileWin.addEventListener("click", addFileWinHandler);
+  generateBillBtn.addEventListener("click", generateBillWinHandler);
+  formDOM.addEventListener("submit", (e) => submitEventHandler(e, formDOM));
 
-  ipcRenderer.on('selected-parts-array', selectedPartsHandler);
+  ipcRenderer.on("selected-parts-array", selectedPartsHandler);
 });
 
 function searchPartWinHandler() {
-  ipcRenderer.send('open-search-window');
+  ipcRenderer.send("open-search-window");
 }
 
 function addFileWinHandler() {
-  ipcRenderer.send('open-add-file-window');
+  ipcRenderer.send("open-add-file-window");
 }
 
 function selectedPartsHandler(e, selectedParts) {
-  const mat = document.querySelector('#material');
-  const mat_desc = document.querySelector('#material_desc');
-  const hsn = document.querySelector('#hsn_code');
-  const mrp = document.querySelector('#price');
-  const qty = document.querySelector('#quantity');
+  const mat = document.querySelector("#material");
+  const mat_desc = document.querySelector("#material_desc");
+  const hsn = document.querySelector("#hsn_code");
+  const mrp = document.querySelector("#price");
+  const qty = document.querySelector("#quantity");
 
-  const matLbl = document.querySelector('#material-label');
-  const mat_descLbl = document.querySelector('#material-desc-label');
-  const hsnLbl = document.querySelector('#hsn-code-label');
-  const mrpLbl = document.querySelector('#price-label');
-  const qtyLbl = document.querySelector('#quantity-label');
+  const matLbl = document.querySelector("#material-label");
+  const mat_descLbl = document.querySelector("#material-desc-label");
+  const hsnLbl = document.querySelector("#hsn-code-label");
+  const mrpLbl = document.querySelector("#price-label");
+  const qtyLbl = document.querySelector("#quantity-label");
 
   selectedParts.forEach((element) => {
     mat.value = element.Material;
     mat.disabled = true;
-    mat_desc.value = element['Material Description'];
+    mat_desc.value = element["Material Description"];
     mat_desc.disabled = true;
-    hsn.value = element['HSN Code'];
+    hsn.value = element["HSN Code"];
     hsn.disabled = true;
     mrp.value = element.MRP;
     mrp.disabled = true;
     qty.value = 1;
-    matLbl.classList.add('active');
-    mat_descLbl.classList.add('active');
-    hsnLbl.classList.add('active');
-    mrpLbl.classList.add('active');
-    qtyLbl.classList.add('active');
+    matLbl.classList.add("active");
+    mat_descLbl.classList.add("active");
+    hsnLbl.classList.add("active");
+    mrpLbl.classList.add("active");
+    qtyLbl.classList.add("active");
   });
 }
 
@@ -68,7 +68,7 @@ function submitEventHandler(e, formDOM) {
   const formElements = formDOM.elements;
 
   let [m, md, hsn, qty, p] = Object.entries(formElements);
-  tableValues.material = m[1].value + '-' + md[1].value;
+  tableValues.material = m[1].value + "-" + md[1].value;
   tableValues.hsn = hsn[1].value;
   tableValues.qty = qty[1].value;
   tableValues.price = p[1].value;
@@ -81,33 +81,36 @@ function submitEventHandler(e, formDOM) {
 }
 
 function createTRandPushToTable() {
-  let table = document.querySelector('#billing-totals > tbody');
+  let table = document.querySelector("#billing-totals > tbody");
 
   // creating table row
   let entries = Object.entries(tableValues);
-  let tr = document.createElement('tr');
+  let tr = document.createElement("tr");
+  const delIcon = createDeleteIcon(tableValues.hsn);
+  const td = document.createElement("td");
   entries.forEach(([k, v]) => {
-    const td = document.createElement('td');
+    const td = document.createElement("td");
     const text = document.createTextNode(v);
     td.appendChild(text);
     tr.appendChild(td);
   });
+  td.appendChild(delIcon);
+  tr.appendChild(td);
   table.appendChild(tr);
 }
 
 function resetForm(form) {
-  var inputs = form.getElementsByTagName('input');
+  var inputs = form.getElementsByTagName("input");
   for (var i = 0; i < inputs.length; i++) {
     switch (inputs[i].type) {
-      // case 'hidden':
-      case 'text':
-        inputs[i].value = '';
+      case "text":
+        inputs[i].value = "";
         inputs[i].disabled = false;
         break;
-      case 'radio':
-      case 'checkbox':
+      case "radio":
+      case "checkbox":
         inputs[i].checked = false;
-      case 'number':
+      case "number":
         inputs[i].value = 0;
         inputs[i].disabled = false;
     }
@@ -115,6 +118,30 @@ function resetForm(form) {
 }
 
 function generateBillWinHandler() {
-  ipcRenderer.send('generate-bill-window', tableData);
-  // tableData = [];
+  console.log(tableData);
+  ipcRenderer.send("generate-bill-window", tableData);
+}
+
+function createDeleteIcon(productID) {
+  const delIcon = document.createElement("i");
+  delIcon.classList.add("material-icons");
+  delIcon.classList.add("pointer");
+  delIcon.innerHTML = "delete";
+  delIcon.id = `delete-icon-${productID}`;
+  delIcon.onclick = (e) => deleteIconHandler(e, productID);
+  return delIcon;
+}
+
+function deleteIconHandler(e, productID) {
+  const table = document.querySelector("#billing-totals > tbody");
+  const tr = e.target.parentNode.parentNode;
+  table.removeChild(tr);
+  if (tableData.length === 1) tableData.pop();
+  else {
+    tableData.forEach((element, index) => {
+      if (element.hsn === productID) {
+        tableData.splice(index, 1);
+      }
+    });
+  }
 }
